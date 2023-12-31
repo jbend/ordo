03dbf246e7d3de8f4846b27495346a0f831fee70
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2023-12-30 19:37:27 -0800

    Added Skeleton UI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import "../app.postcss";
+
+  import { AppShell } from '@skeletonlabs/skeleton';
+  import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- svelte-ignore missing-declaration -->
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+  <svelte:fragment slot="sidebarLeft">
+    <!-- Insert the list: -->
+    <nav class="list-nav">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+      <LightSwitch />
+
+    </nav>
+    <!-- --- -->
+  </svelte:fragment>
+
+  <slot />
+
+</AppShell>
